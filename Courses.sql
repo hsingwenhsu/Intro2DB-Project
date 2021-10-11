e1a5686dfec960d7a213c401ac12508ab34a1712
hsingwenhsu <b05502012@ntu.edu.tw>
2021-10-11 04:04:58 -0400

    fix data

--- Courses.sql
+++ Courses.sql
@@ -10,31 +10,31 @@ CREATE TABLE Courses(
 insert into Courses (cnum, did, name, credits)
 values ('E6156', 'COMS', 'CLOUD COMPUTING', 3);
 
-insert into Courses (cnum, did, name, description, credits) 
+insert into Courses (cnum, did, name, credits) 
 values ('W4111', 'COMS', 'INTRODUCTION TO DATABASES', 3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4115', 'COMS', 'PROGRAMMING LANG & TRANSLATORS', 3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4118', 'COMS', 'OPERATING SYSTEMS I', 3);
 
-insert into Courses (cnum, did, name, description, credits) 
+insert into Courses (cnum, did, name, credits) 
 values ('W4156', 'COMS', 'ADVANCED SOFTWARE ENGINEERING', 3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4167', 'COMS', 'COMPUTER ANIMATION',3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4186', 'COMS', 'MALWARE ANALYSIS&REVERSE ENGINEERING',3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4252', 'COMS', 'INTRO-COMPUTATIONAL LEARN THRY',3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4170', 'COMS', 'USER INTERFACE DESIGN',3);
 
-insert into Courses (cnum, did, name, description, credits)
+insert into Courses (cnum, did, name, credits) 
 values ('W4181', 'COMS', 'SECURITY I', 3);
 
 
